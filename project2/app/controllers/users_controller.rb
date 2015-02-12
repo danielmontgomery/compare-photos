4b3cf2935f578ecb5b0343586bc6e7a6633465f5
danielmontgomery <daniel.w.montgomery@gmail.com>
2015-02-11 16:57:51 -0800

    added very basic paperclip functions

---
+++ project2/app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(params.require(:user).permit(:username, :email, :avatar))
+    if user.save
+      redirect_to users_path
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+end
